922913a9db9bc63036d5463f90c0b1ca00f1c26a
Kit Kogan <kkogan.dev@gmail.com>
2020-06-14 16:38:40 -0500

    practice more databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
+  userName = '';
 
   constructor() {
     setTimeout(() => {
@@ -25,5 +26,9 @@ export class ServersComponent implements OnInit {
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetUsername() {
+    this.userName = '';
   }
 }
